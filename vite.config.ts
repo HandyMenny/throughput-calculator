d5818b4983d57a533a63402d4b7bd7076fedae96
handymenny <handymenny@outlook.com>
2024-03-03 16:35:40 +0100

    add .env files

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
-import { defineConfig, type UserConfig } from "vite";
+import { defineConfig, loadEnv, type UserConfig } from "vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig((): UserConfig => {
+export default defineConfig(({mode}): UserConfig => {
+  const env = loadEnv(mode, process.cwd(), "");
   return {
+    base: env.PUBLIC_BASE_PATH ?? '/',
     plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
     server: {
       headers: {
