736c8bfd79fbc1b30174ed4633f5f0de7fb85aa6
handymenny <handymenny@outlook.com>
2024-03-08 13:25:25 +0100

    modulation-nr: add manual code rate + order

--- src/components/feature/modulation-nr.tsx
+++ src/components/feature/modulation-nr.tsx
@@ -9,6 +9,7 @@ import SelectInput from '../input/select-input';
 import { getModulation } from '~/helpers/calculator';
 import type { McsTablesNR, Modulation } from '~/@types/layer-nr';
 import { mcstables } from '~/helpers/db/mcstables';
+import NumberInput from '../input/number-input';
 
 interface Props {
   selectedValue?: Signal<Modulation>;
@@ -21,9 +22,29 @@ interface Props {
 export default component$((props: Props) => {
   const { prefix, selectedValue, ul, dft, hidden } = props;
 
-  const selectedMod = useSignal<string>('');
+  const selectedMod = useSignal<string>('6');
   const selectedMcsTable = useSignal<McsTablesNR>('qam64');
-  const selectedMcsIndex = useSignal<string>('');
+  const selectedMcsIndex = useSignal<string>('0');
+  const selectedModulationOrder = useSignal<string>('6');
+  const selectedCodeRate = useSignal<number>(0.92578125);
+
+  const modulationOrderOptions = useComputed$(() => {
+    const arr = [
+      { label: '2 (QPSK)', value: '2' },
+      { label: '4 (16QAM)', value: '4' },
+      { label: '6 (64QAM)', value: '6' },
+      { label: '8 (256QAM)', value: '8' },
+    ];
+
+    if (!ul) {
+      const qam1024 = { label: '10 (1024QAM)', value: '10' };
+      arr.splice(4, 0, qam1024);
+    } else if (dft) {
+      const bpsk = { label: '1 (π/2 BPSK)', value: '1' };
+      arr.splice(0, 0, bpsk);
+    }
+    return arr;
+  });
 
   const modulationOptions = useComputed$(() => {
     const arr = [
@@ -32,6 +53,7 @@ export default component$((props: Props) => {
       { label: '64QAM', value: '6' },
       { label: '256QAM', value: '8' },
       { label: 'MCS index', value: '-1' },
+      { label: 'Modulation Order + Code rate', value: '-2' },
     ];
 
     if (!ul) {
@@ -86,12 +108,20 @@ export default component$((props: Props) => {
     track(() => selectedMod.value);
     track(() => selectedMcsTable.value);
     track(() => selectedMcsIndex.value);
+    track(() => selectedModulationOrder.value);
+    track(() => selectedCodeRate.value);
+
     if (selectedValue == null) return;
 
     let mod;
     if (selectedMod.value == '-1') {
       const mcsTable = selectedMcsTable.value;
       mod = getModulation(parseInt(selectedMcsIndex.value), mcsTable);
+    } else if (selectedMod.value == '-2') {
+      mod = {
+        modOrder: parseInt(selectedModulationOrder.value),
+        codeRate: selectedCodeRate.value,
+      };
     } else {
       mod = {
         modOrder: parseInt(selectedMod.value),
@@ -131,6 +161,19 @@ export default component$((props: Props) => {
         options={mcsIndexes.value}
         selectedValue={selectedMcsIndex}
         hidden={hidden || selectedMod.value !== '-1'}
+      />
+      <SelectInput
+        label={`${prefix} Modulation Order`}
+        labelClass="text-center"
+        options={modulationOrderOptions.value}
+        selectedValue={selectedModulationOrder}
+        hidden={hidden || selectedMod.value !== '-2'}
+      />
+      <NumberInput
+        label={`${prefix} Code Rate`}
+        labelClass="text-center"
+        selectedValue={selectedCodeRate}
+        hidden={hidden || selectedMod.value !== '-2'}
       />
     </>
   );
