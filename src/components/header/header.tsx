ee10c0288e836aa32d55d3d2f8397f44fdc761dc
handymenny <handymenny@outlook.com>
2024-03-10 15:10:58 +0100

    fix home url

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,13 +4,14 @@ import { Link } from '@builder.io/qwik-city';
 export default component$(() => {
   const credit = import.meta.env.PUBLIC_CREDIT;
   const creditUrl = import.meta.env.PUBLIC_CREDIT_URL;
+  const basePath = import.meta.env.PUBLIC_BASE_PATH || '/';
 
   return (
     <header class="flex justify-between bg-black px-5 py-2">
       <div>
         <Link
           class="text-xl text-white hover:underline active:underline"
-          href="/"
+          href={basePath}
           title="Throughput Calculator"
         >
           Throughput Calculator
